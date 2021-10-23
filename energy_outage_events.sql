4007715fb4c09c6fc2b54c4a359bb55f5b4050f6
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2021-10-23 15:58:38 -0700

    Delete energy_outage_events.sql

--- energy_outage_events.sql
+++
@@ -1,9 +0,0 @@
--- Your answer here
-SELECT 
-  COUNT(status) AS Total_Number_Outage_Events,
-  Status,
-  Reason
-FROM AEMR
-WHERE YEAR(Start_Time) ='2016' AND Status = 'Approved'
-GROUP BY reason
-ORDER BY reason 
