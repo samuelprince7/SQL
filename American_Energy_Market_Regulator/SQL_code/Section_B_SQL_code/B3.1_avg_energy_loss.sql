84f84ab4852cdcf6e7923b7ae958933084ce7d5e
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2022-03-09 16:57:04 -0800

    Update B3.1_avg_energy_loss.sql

--- American_Energy_Market_Regulator/SQL_code/Section_B_SQL_code/B3.1_avg_energy_loss.sql
+++ American_Energy_Market_Regulator/SQL_code/Section_B_SQL_code/B3.1_avg_energy_loss.sql
@@ -4,5 +4,5 @@ SELECT Participant_Code, Status, Year(Start_Time) AS Year,
 
 FROM AEMR
 WHERE Status='Approved' AND Reason ='Forced'
-GROUP BY Participant_Code, Status, Reason, Year(Start_Time)
+GROUP BY Participant_Code, Status, Year(Start_Time)
 ORDER BY Year(Start_Time) ASC, ROUND(AVG(Outage_MW),2) DESC;
