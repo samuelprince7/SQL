5aff1e5e684acb72a7808cdb2c61786d2c8635fd
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2022-03-09 16:11:04 -0800

    Update Q1_SQL_Solution.sql

--- Video_Game_Sales_Data/SQL_Solutions/Q1_SQL_Solution.sql
+++ Video_Game_Sales_Data/SQL_Solutions/Q1_SQL_Solution.sql
@@ -1,5 +1,6 @@
 SELECT publisher, ROUND(SUM(global_sales),2) 
 FROM VG_Sales
 GROUP BY publisher
-ORDER BY SUM(global_sales) DESC LIMIT 10
+ORDER BY SUM(global_sales) DESC 
+LIMIT 10
                       
