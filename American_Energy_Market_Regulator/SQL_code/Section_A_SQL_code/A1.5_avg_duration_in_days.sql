44fcabe611ec51bd5d76173782e4663ff2e811b6
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2022-03-09 16:36:26 -0800

    Update A1.5_avg_duration_in_days.sql

--- American_Energy_Market_Regulator/SQL_code/Section_A_SQL_code/A1.5_avg_duration_in_days.sql
+++ American_Energy_Market_Regulator/SQL_code/Section_A_SQL_code/A1.5_avg_duration_in_days.sql
@@ -1,6 +1,6 @@
 SELECT 
-  Status, Reason, Count(*) AS Total_Number_Outage_Events,
-  ROUND(((avg(TIMESTAMPDIFF(Minute,Start_Time,End_Time)))/60)/24,2) AS Average_Outage_Duration_Time_Days,
+  Status, Reason, COUNT(*) AS Total_Number_Outage_Events,
+  ROUND(((AVG(TIMESTAMPDIFF(Minute,Start_Time,End_Time)))/60)/24,2) AS Average_Outage_Duration_Time_Days,
   YEAR(Start_Time) AS Year
 
 FROM AEMR
